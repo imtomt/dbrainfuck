22da9f3664fc5a2a547254c86e526b55c6c6903a
tomt </dev/null>
2025-04-01 06:13:48 -0700

    i forgor dbf.h

--- dbf.h
+++ dbf.h
@@ -4,28 +4,28 @@
 #include <stdio.h>
 
 #define TAPE_LEN 30000 // change this to increase/decrase tape length
-//#define dbf_default { 0, 0, NULL, 0 }
+                       //#define dbf_default { 0, 0, NULL, 0 }
 #define DEBUG 0
 
 #if defined(DEBUG) && DEBUG > 0
- #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
-    __FILE__, __LINE__, __func__, ##args)
+#define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+        __FILE__, __LINE__, __func__, ##args)
 #else
- #define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
+#define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
 #endif
 
 typedef enum {
-	SUCCESS,
-	FILE_ERROR,
-	MEM_ERROR,
-	OTHER_ERROR
+    SUCCESS,
+    FILE_ERROR,
+    MEM_ERROR,
+    OTHER_ERROR
 } Status;
 
 struct dbf_t {
-	int num_lines;
-	int line_len;
-	char **lines;
-	Status status;
+    int num_lines;
+    int line_len;
+    char **lines;
+    Status status;
 };
 
 int count_lines(FILE *);
