f1e5c02e990f27c441522cff0cf188a2d546b083
tomt </dev/null>
2025-04-01 06:12:49 -0700

    sorry, changed indentation from 1 tab character to 4 spaces... im really sorry.......fuck...........nothing else changed this commit i SWEAR

--- file.c
+++ file.c
@@ -9,40 +9,40 @@
  */
 struct dbf_t init_dbf()
 {
-	struct dbf_t dbf;
-	dbf.num_lines = 0;
-	dbf.line_len = 0;
-	dbf.lines = NULL;
-	dbf.status = SUCCESS;
+    struct dbf_t dbf;
+    dbf.num_lines = 0;
+    dbf.line_len = 0;
+    dbf.lines = NULL;
+    dbf.status = SUCCESS;
 
-	return dbf;
+    return dbf;
 }
 
 /*
  * Return the number of lines from a file given as argument */
 int count_lines(FILE *fp)
 {
-	int lines = 0;
-	int ch = 0;
+    int lines = 0;
+    int ch = 0;
 
-	for (ch = getc(fp); ch != EOF; ch = getc(fp)) {
-		if (ch == '\n')
-			lines += 1;
-	}
+    for (ch = getc(fp); ch != EOF; ch = getc(fp)) {
+        if (ch == '\n')
+            lines += 1;
+    }
 
-	rewind(fp);
-	return lines;
+    rewind(fp);
+    return lines;
 }
 
 /* dbf.lines gets alloced and realloced, so we need to
  * free everything. */
 int free_dbf(struct dbf_t dbf)
 {
-	for (int i = 0; i < dbf.num_lines; i++)
-		free(dbf.lines[i]);
-	free(dbf.lines);
+    for (int i = 0; i < dbf.num_lines; i++)
+        free(dbf.lines[i]);
+    free(dbf.lines);
 
-	return 0;
+    return 0;
 }
 
 /* Read from a file, given as a string argument, and process for dbf.
@@ -54,90 +54,90 @@ int free_dbf(struct dbf_t dbf)
  * through lines. */
 struct dbf_t read_dbf_from_file(char *file)
 {
-	struct dbf_t result = init_dbf();
-	FILE *fp = fopen(file, "r");
-	if (fp == NULL) {
-		DEBUG_PRINT("*** Could not open file %s.\n", file);
-		result.status = FILE_ERROR;
-		return result;
-	}
+    struct dbf_t result = init_dbf();
+    FILE *fp = fopen(file, "r");
+    if (fp == NULL) {
+        DEBUG_PRINT("*** Could not open file %s.\n", file);
+        result.status = FILE_ERROR;
+        return result;
+    }
 
-	int num_lines = count_lines(fp);
-	/* Allocate enough space for the number of lines. Each line
-	 * will be malloc'ed to fit the line length later. */
-	char **lines = (char **)malloc(num_lines * sizeof(char *));
+    int num_lines = count_lines(fp);
+    /* Allocate enough space for the number of lines. Each line
+     * will be malloc'ed to fit the line length later. */
+    char **lines = (char **)malloc(num_lines * sizeof(char *));
 
-	if (lines == NULL) {
-		DEBUG_PRINT("*** Could not allocate memory.\n");
-		result.status = MEM_ERROR;
-		return result;
-	}
+    if (lines == NULL) {
+        DEBUG_PRINT("*** Could not allocate memory.\n");
+        result.status = MEM_ERROR;
+        return result;
+    }
 
-	char buf[1024];
-	int len = 0;
-	int maxlen = 0;
+    char buf[1024];
+    int len = 0;
+    int maxlen = 0;
 
-	DEBUG_PRINT("Reading lines\n");
+    DEBUG_PRINT("Reading lines\n");
 
-	/* Read each line of the file to get the longest line length, so we can
-	   pad each line in the future. */
-	for (int i = 0; i < num_lines; i++) {
-		if (fgets(buf, sizeof(buf), fp) != NULL) {
-			len = strlen(buf);
-			if (len > maxlen)
-				maxlen = len;
-		}
-		else if (feof(fp)) {
-			break;
-		}
-		else {
-			DEBUG_PRINT("*** File error\n");
-			result.status = FILE_ERROR;
-			return result;
-		}
-	}
+    /* Read each line of the file to get the longest line length, so we can
+       pad each line in the future. */
+    for (int i = 0; i < num_lines; i++) {
+        if (fgets(buf, sizeof(buf), fp) != NULL) {
+            len = strlen(buf);
+            if (len > maxlen)
+                maxlen = len;
+        }
+        else if (feof(fp)) {
+            break;
+        }
+        else {
+            DEBUG_PRINT("*** File error\n");
+            result.status = FILE_ERROR;
+            return result;
+        }
+    }
 
-	// Rewind the file to start at the beginning
-	fseek(fp, 0, SEEK_SET);
+    // Rewind the file to start at the beginning
+    fseek(fp, 0, SEEK_SET);
 
-	/* Pad each line to the same length, allocating just enough memory. */
-	for (int i = 0; i < num_lines; i++) {
-		if (fgets(buf, sizeof(buf), fp) != NULL) {
-			len = strlen(buf);
+    /* Pad each line to the same length, allocating just enough memory. */
+    for (int i = 0; i < num_lines; i++) {
+        if (fgets(buf, sizeof(buf), fp) != NULL) {
+            len = strlen(buf);
 
-			/* Replace newlines with NULL, we don't want \n in our
-			   actual tape. */
-			if (buf[len-1] == '\n')
-				buf[len-1] = ' ';
+            /* Replace newlines with NULL, we don't want \n in our
+               actual tape. */
+            if (buf[len-1] == '\n')
+                buf[len-1] = ' ';
 
-			lines[i] = (char *)malloc((maxlen + 1) * sizeof(char));
-			if (lines == NULL) {
-				DEBUG_PRINT("*** Could not allocate memory.\n");
-				result.status = MEM_ERROR;
-				return result;
-			}
+            lines[i] = (char *)malloc((maxlen + 1) * sizeof(char));
+            if (lines == NULL) {
+                DEBUG_PRINT("*** Could not allocate memory.\n");
+                result.status = MEM_ERROR;
+                return result;
+            }
 
-			strncpy(lines[i], buf, len + 1);
+            strncpy(lines[i], buf, len + 1);
 
-			/* Pad the lines to all the same length, using spaces */
-			for (int c = len; c < maxlen; c++)
-				lines[i][c] = ' ';
+            /* Pad the lines to all the same length, using spaces */
+            for (int c = len; c < maxlen; c++)
+                lines[i][c] = ' ';
 
-			lines[i][maxlen] = '\0';
-		}
-		else {
-			DEBUG_PRINT("*** Unexpected EOF\n");
-			result.status = FILE_ERROR;
-			return result;
-		}
-	}
+            lines[i][maxlen] = '\0';
+        }
+        else {
+            DEBUG_PRINT("*** Unexpected EOF\n");
+            result.status = FILE_ERROR;
+            return result;
+        }
+    }
 
-	fclose(fp);
+    fclose(fp);
 
-	/* Populate the dbf struct */
-	result.num_lines = num_lines;
-	result.line_len = maxlen;
-	result.lines = lines;
+    /* Populate the dbf struct */
+    result.num_lines = num_lines;
+    result.line_len = maxlen;
+    result.lines = lines;
 
-	return result;
+    return result;
 }
--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 #include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdlib.h>
+//#include <string.h>
 #include <ncurses.h>
 
 #define MAX_STEP 4
@@ -11,260 +11,260 @@
 /* Print a map of the field, displaying an 'x' for our current position. */
 void dbfbug(struct dbf_t dbf, int x, int y, int ti, int tv, char d)
 {
-	char c;
-	int delay = 35;
-	static int step = 0;
+    char c;
+    int delay = 35;
+    static int step = 0;
 
-	init_pair(1, COLOR_RED, -1);
-	init_pair(2, COLOR_GREEN, -1);
-	init_pair(3, 9, -1);
-	init_pair(4, 7, -1);
+    init_pair(1, COLOR_RED, -1);
+    init_pair(2, COLOR_GREEN, -1);
+    init_pair(3, 9, -1);
+    init_pair(4, 7, -1);
 
-	erase();
-	move(0, 0);
+    erase();
+    move(0, 0);
 
-	attron(COLOR_PAIR(2));
-	printw("Tape Index:\n");
-	printw("Current Cell:\n");
-	printw("Direction:\n");
-	printw("Coords (x, y):\n");
-	printw("Output:");
-	attroff(COLOR_PAIR(2));
+    attron(COLOR_PAIR(2));
+    printw("Tape Index:\n");
+    printw("Current Cell:\n");
+    printw("Direction:\n");
+    printw("Coords (x, y):\n");
+    printw("Output:");
+    attroff(COLOR_PAIR(2));
 
-	attron(COLOR_PAIR(1));
-	attron(A_BOLD);
-	mvprintw(0, 17, "%d", ti);
-	mvprintw(1, 17, "%d", tv);
-	mvprintw(2, 17, "%c", d);
-	mvprintw(3, 17, "(%d, %d)", x, y);
+    attron(COLOR_PAIR(1));
+    attron(A_BOLD);
+    mvprintw(0, 17, "%d", ti);
+    mvprintw(1, 17, "%d", tv);
+    mvprintw(2, 17, "%c", d);
+    mvprintw(3, 17, "(%d, %d)", x, y);
 
-	/* If we try to print normally while in debug mode with ncurses enabled,
-	 * it fucks up the formatting, and skews everything. Unfortunately we
-	 * have to handle '.' differently if we are in debug mode. */
-	if (dbf.lines[y][x] == '.') {
-		// print the character representation of the cell if able...
-		if (isprint(tv))
-			mvprintw(4, 17, "'%c'", tv);
-		// ... otherwise print the hex value
-		else
-			mvprintw(4, 17, "0x%X", tv);
-	}
-	else
-		mvprintw(4, 17, "(none)");
+    /* If we try to print normally while in debug mode with ncurses enabled,
+     * it fucks up the formatting, and skews everything. Unfortunately we
+     * have to handle '.' differently if we are in debug mode. */
+    if (dbf.lines[y][x] == '.') {
+        // print the character representation of the cell if able...
+        if (isprint(tv))
+            mvprintw(4, 17, "'%c'", tv);
+        // ... otherwise print the hex value
+        else
+            mvprintw(4, 17, "0x%X", tv);
+    }
+    else
+        mvprintw(4, 17, "(none)");
 
-	attroff(A_BOLD);
-	attroff(COLOR_PAIR(1));
+    attroff(A_BOLD);
+    attroff(COLOR_PAIR(1));
 
-	/* Scrolling window debug, useful for programs larger than 1 page */
-	move(5, 0);
-	for (int i = (dbf.num_lines > LINES-10) ? y/2 : 0; i < dbf.num_lines; i++) {
-		for (int n = 0; n < dbf.line_len; n++) {
-			if (i == y && n == x) {
-				attron(COLOR_PAIR(3));
-				attron(A_BOLD);
-				addch('X');
-				attroff(COLOR_PAIR(3));
-				attroff(A_BOLD);
-			}
-			else {
-				attron(COLOR_PAIR(4));
-				addch(dbf.lines[i][n]);
-				attroff(COLOR_PAIR(4));
-			}
-		}
-		addch('\n');
-	}
+    /* Scrolling window debug, useful for programs larger than 1 page */
+    move(5, 0);
+    for (int i = (dbf.num_lines > LINES-10) ? y/2 : 0; i < dbf.num_lines; i++) {
+        for (int n = 0; n < dbf.line_len; n++) {
+            if (i == y && n == x) {
+                attron(COLOR_PAIR(3));
+                attron(A_BOLD);
+                addch('X');
+                attroff(COLOR_PAIR(3));
+                attroff(A_BOLD);
+            }
+            else {
+                attron(COLOR_PAIR(4));
+                addch(dbf.lines[i][n]);
+                attroff(COLOR_PAIR(4));
+            }
+        }
+        addch('\n');
+    }
 
-	move(LINES-3, 0);
-	attron(COLOR_PAIR(2));
-	attron(A_BOLD);
-	printw("Press `r` to automatically step through.\n");
-	printw("Press `r` again to cycle speeds..\n");
-	printw("Press any key to continue.\n");
-	if (step == 0)
-		mvprintw(LINES-1, COLS-15, "Manual stepping");
-	else
-		mvprintw(LINES-1, COLS-14, "Autostep (%d/%d)", step, MAX_STEP);
-	attroff(A_BOLD);
-	attroff(COLOR_PAIR(2));
-	refresh();
+    move(LINES-3, 0);
+    attron(COLOR_PAIR(2));
+    attron(A_BOLD);
+    printw("Press `r` to automatically step through.\n");
+    printw("Press `r` again to cycle speeds..\n");
+    printw("Press any key to continue.\n");
+    if (step == 0)
+        mvprintw(LINES-1, COLS-15, "Manual stepping");
+    else
+        mvprintw(LINES-1, COLS-14, "Autostep (%d/%d)", step, MAX_STEP);
+    attroff(A_BOLD);
+    attroff(COLOR_PAIR(2));
+    refresh();
 
-	if (step == 0) {
-		timeout(-1);
-		c = getch();
-		if (c == 'r') {
-			step = 1;
-		}
-	}
-	else {
-		timeout(delay/step);
-		c = getch();
-		if (c == 'r') {
-			if (step == MAX_STEP)
-				step = 0;
-			else
-				step += 1;
-		}
-	}
+    if (step == 0) {
+        timeout(-1);
+        c = getch();
+        if (c == 'r') {
+            step = 1;
+        }
+    }
+    else {
+        timeout(delay/step);
+        c = getch();
+        if (c == 'r') {
+            if (step == MAX_STEP)
+                step = 0;
+            else
+                step += 1;
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	struct dbf_t dbf;
-	int debug;
+    struct dbf_t dbf;
+    int debug;
 
-	if (argc == 1) {
-		printf("Usage: ./%s [file] (-d)", argv[0]);
-		return 1;
-	}
+    if (argc == 1) {
+        printf("Usage: ./%s [file] (-d)", argv[0]);
+        return 1;
+    }
 
-	/* TODO: *actually* check if argv[2] is "-d", currently any
-	 * option will enable debug mode. */
-	if (argc > 2)
-		debug = 1;
-	else
-		debug = 0;
+    /* TODO: *actually* check if argv[2] is "-d", currently any
+     * option will enable debug mode. */
+    if (argc > 2)
+        debug = 1;
+    else
+        debug = 0;
 
-	/* from file.c
-	 * Read from the first argument into a dbf_t struct */
-	dbf = read_dbf_from_file(argv[1]);
-	if (dbf.status != SUCCESS) {
-		printf("An error occured.\nError code: %d\n", dbf.status);
-		free_dbf(dbf);
-		return 1;
-	}
+    /* from file.c
+     * Read from the first argument into a dbf_t struct */
+    dbf = read_dbf_from_file(argv[1]);
+    if (dbf.status != SUCCESS) {
+        printf("An error occured.\nError code: %d\n", dbf.status);
+        free_dbf(dbf);
+        return 1;
+    }
 
-	DEBUG_PRINT("Initializing vars\n");
-	char tape[TAPE_LEN] = { 0 }; /* TAPE_LEN from dbf.h */
-	int pointer = 0;
-	int x = 0;
-	int y = 0;
-	char direction = '>';
-	char t;
+    DEBUG_PRINT("Initializing vars\n");
+    char tape[TAPE_LEN] = { 0 }; /* TAPE_LEN from dbf.h */
+    int pointer = 0;
+    int x = 0;
+    int y = 0;
+    char direction = '>';
+    char t;
 
-	/* Set up ncurses if the debug flag is passed on command line. */
-	DEBUG_PRINT("Initializing ncurses\n");
-	if (debug) {
-		initscr();
-		use_default_colors();
-		start_color();
-		curs_set(0);
-	}
+    /* Set up ncurses if the debug flag is passed on command line. */
+    DEBUG_PRINT("Initializing ncurses\n");
+    if (debug) {
+        initscr();
+        use_default_colors();
+        start_color();
+        curs_set(0);
+    }
 
-	/* Main logic behind directional brainfuck :3 */
-	DEBUG_PRINT("Loop begin.\n");
-	for (;;) {
-		/* We need - 1, because it will attempt to access out of bounds
-		 * due to arrays being 0-indexed. This causes a segfault. */
-		if (y > dbf.num_lines - 1 ||
-		    y < 0 ||
-		    x > dbf.line_len - 1 ||
-		    x < 0) {
-			DEBUG_PRINT("*** Outta bounds:\n");
-			DEBUG_PRINT("y: %d\n", y);
-			DEBUG_PRINT("x: %d\n", x);
-			DEBUG_PRINT("direction: %c\n", direction);
-			break;
-		}
+    /* Main logic behind directional brainfuck :3 */
+    DEBUG_PRINT("Loop begin.\n");
+    for (;;) {
+        /* We need - 1, because it will attempt to access out of bounds
+         * due to arrays being 0-indexed. This causes a segfault. */
+        if (y > dbf.num_lines - 1 ||
+                y < 0 ||
+                x > dbf.line_len - 1 ||
+                x < 0) {
+            DEBUG_PRINT("*** Outta bounds:\n");
+            DEBUG_PRINT("y: %d\n", y);
+            DEBUG_PRINT("x: %d\n", x);
+            DEBUG_PRINT("direction: %c\n", direction);
+            break;
+        }
 
-		t = dbf.lines[y][x];
+        t = dbf.lines[y][x];
 
-		/* 'e' allows the dbf program to specify an exit point, which is
-		 * pretty cool. */
-		if (t == 'e')
-			break;
+        /* 'e' allows the dbf program to specify an exit point, which is
+         * pretty cool. */
+        if (t == 'e')
+            break;
 
-		/* Interpret directional brainfuck :3 */
-		switch (t) {
-		case '^':
-		case 'v':
-		case '>':
-		case '<':
-			direction = t;
-			break;
-		/* Conditional. If the current cell is 0, direction becomes,
-		 * otherwise v. */
-		case '?':
-			if (tape[pointer] == 0)
-				direction = '^';
-			else
-				direction = 'v';
-			break;
-		case '}':
-			if (pointer >= TAPE_LEN - 1)
-				pointer = 0;
-			else
-				pointer += 1;
-			break;
-		case '{':
-			if (pointer == 0)
-				pointer = TAPE_LEN - 1;
-			else
-				pointer -= 1;
-			break;
-		case '+':
-			tape[pointer] += 1;
-			break;
-		case '-':
-			tape[pointer] -= 1;
-			break;
-		case '.':
-			/* Printing while in debug mode screws up the whole
-			   debug view, offsetting the rest of the screen, so
-			   dbfbug() will handle printing while in debug mode */
-			if (!debug)
-				putchar(tape[pointer]);
-			break;
-		case ',':
-			if (t == ',')
-				tape[pointer] = getchar();
-			break;
+        /* Interpret directional brainfuck :3 */
+        switch (t) {
+        case '^':
+        case 'v':
+        case '>':
+        case '<':
+            direction = t;
+            break;
+            /* Conditional. If the current cell is 0, direction becomes,
+             * otherwise v. */
+        case '?':
+            if (tape[pointer] == 0)
+                direction = '^';
+            else
+                direction = 'v';
+            break;
+        case '}':
+            if (pointer >= TAPE_LEN - 1)
+                pointer = 0;
+            else
+                pointer += 1;
+            break;
+        case '{':
+            if (pointer == 0)
+                pointer = TAPE_LEN - 1;
+            else
+                pointer -= 1;
+            break;
+        case '+':
+            tape[pointer] += 1;
+            break;
+        case '-':
+            tape[pointer] -= 1;
+            break;
+        case '.':
+            /* Printing while in debug mode screws up the whole
+               debug view, offsetting the rest of the screen, so
+               dbfbug() will handle printing while in debug mode */
+            if (!debug)
+                putchar(tape[pointer]);
+            break;
+        case ',':
+            if (t == ',')
+                tape[pointer] = getchar();
+            break;
 
-		case 'd':
-		/* Toggle debug mode. If we're currently in debug mode, a 'd'
-		 * in the file will disable it, and vice versa. We should set
-		 * up and end ncurses as needed. */
-			if (debug)
-				endwin();
-			else {
-				initscr();
-				use_default_colors();
-				start_color();
-			}
+        case 'd':
+            /* Toggle debug mode. If we're currently in debug mode, a 'd'
+             * in the file will disable it, and vice versa. We should set
+             * up and end ncurses as needed. */
+            if (debug)
+                endwin();
+            else {
+                initscr();
+                use_default_colors();
+                start_color();
+            }
 
-			debug = !debug;
-			break;
-		default:
-			break;
-		}
+            debug = !debug;
+            break;
+        default:
+            break;
+        }
 
-		/* Move forward one space in whichever direction. */
-		if (direction == '^')
-			y -= 1;
-		else if (direction == 'v')
-			y += 1;
-		else if (direction == '>')
-			x += 1;
-		else if (direction == '<')
-			x -= 1;
-		else {
-			DEBUG_PRINT("Unexpected direction: '%c' (0x%x)\n",
-				    direction, direction);
-			break;
-		}
+        /* Move forward one space in whichever direction. */
+        if (direction == '^')
+            y -= 1;
+        else if (direction == 'v')
+            y += 1;
+        else if (direction == '>')
+            x += 1;
+        else if (direction == '<')
+            x -= 1;
+        else {
+            DEBUG_PRINT("Unexpected direction: '%c' (0x%x)\n",
+                    direction, direction);
+            break;
+        }
 
-		if (debug)
-			dbfbug(dbf, x, y, pointer, tape[pointer], direction);
-	}
+        if (debug)
+            dbfbug(dbf, x, y, pointer, tape[pointer], direction);
+    }
 
-	DEBUG_PRINT("Freeing...\n");
-	free_dbf(dbf); /* from file.c */
-	/* End ncurses shit */
-	if (debug) {
-		curs_set(1);
-		endwin();
-	}
-	DEBUG_PRINT("Done.\n");
+    DEBUG_PRINT("Freeing...\n");
+    free_dbf(dbf); /* from file.c */
+    /* End ncurses shit */
+    if (debug) {
+        curs_set(1);
+        endwin();
+    }
+    DEBUG_PRINT("Done.\n");
 
-	return 0;
+    return 0;
 }
